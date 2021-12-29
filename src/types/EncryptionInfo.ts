9eb1eed33708d0bd87da6aee1d02a1197e832c9a
wbydc <root@wbydc.io>
2021-12-29 16:09:04 +0300

    extend EncryptionDependencies

--- src/types/EncryptionInfo.ts
+++ src/types/EncryptionInfo.ts
@@ -1,8 +1,9 @@
 import { CryptoAlgorithm, Encoding } from '../enum';
+import { EncryptionDependencies } from './EncryptionDependencies';
 
 export interface EncryptionInfo {
     algo: CryptoAlgorithm;
     encoding: Encoding;
     key: string;
-    dependencies?: string;
+    dependencies: EncryptionDependencies;
 }
