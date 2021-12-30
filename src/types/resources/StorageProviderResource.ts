4f037d373a42ed33dbac5f6ca3794d657a52199e
wbydc <root@wbydc.io>
2021-12-30 13:56:38 +0300

    Fix StorageProviderResource

--- src/types/resources/StorageProviderResource.ts
+++ src/types/resources/StorageProviderResource.ts
@@ -2,9 +2,7 @@ import { StorageType } from '../../enum';
 import { Resource } from './Resource';
 
 export interface StorageProviderResource extends Resource {
-    access: {
-        storageType: StorageType;
-        filepath: string;
-        credentials: any;
-    };
+    storageType: StorageType;
+    filepath: string;
+    credentials: any;
 }
